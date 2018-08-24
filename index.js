6d91bb653a661923be1fe474ac674bbd3a1d0ea8
Soumyadeep m.soumyadeep@gmail.com <Soumyadeep m.soumyadeep@gmail.com>
2018-08-24 11:54:39 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () => {
+	return (
+		<View style={{ flex: 1 }}>
+			<Header headerText={'ALBUMS'} />
+			<AlbumList />
+		</View>	
+	);	
+};
+
+AppRegistry.registerComponent('albums', () => App);
